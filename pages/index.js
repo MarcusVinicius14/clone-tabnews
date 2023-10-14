66c72bccd11ac7176fa24a371f938825e1efa94c
Marcus Vinicius Assunção Silva <114960665+MarcusVinicius14@users.noreply.github.com>
2023-10-14 15:55:28 +0000

    adiciona atualizacao na pagina e uma pasta de styles

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,17 @@
+import styles from "../styles/home.module.css"
 function Home () {
+
+function sim() {
+    alert("Tambem te amo! <3 :)");
+}
     return (
-        <h1>Momo eu amo você, se voce me ama da uma risadinha!</h1>
+        <div className={styles.conteudo}>
+            <h1>Você ama eu?</h1>
+            <div className={styles.containerButton}>
+                <button onClick={sim} className={styles.btn1}>Sim</button>
+                <button className={styles.btn2}>Não</button>
+            </div>
+        </div>
     )
 }
 
