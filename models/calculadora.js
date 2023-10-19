3fc270fe7558f2b9f98f22df5d76f5b6ac414b24
Marcus Vinicius Assunção Silva <114960665+MarcusVinicius14@users.noreply.github.com>
2023-10-19 22:38:26 +0000

    troca conteudo principal

---
+++ models/calculadora.js
@@ -0,0 +1,5 @@
+function soma(arg1, arg2) {
+  return arg1 + arg2;
+}
+
+exports.soma = soma;
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,5 @@
-import styles from "../styles/home.module.css";
 function Home() {
-  function sim() {
-    alert("Tambem te amo! <3 :)");
-  }
-  return (
-    <div className={styles.conteudo}>
-      <h1>Você ama eu?</h1>
-      <div className={styles.containerButton}>
-        <button onClick={sim} className={styles.btn1}>
-          Sim
-        </button>
-        <button className={styles.btn2}>Não</button>
-      </div>
-    </div>
-  );
+  return <div>Projeto em construção</div>;
 }
 
 export default Home;
