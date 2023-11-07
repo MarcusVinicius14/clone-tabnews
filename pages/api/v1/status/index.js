ca9c5a58c8fc4edc1a002bf3448c917a66f6148e
Marcus Vinicius Assunção Silva <114960665+MarcusVinicius14@users.noreply.github.com>
2023-11-07 17:15:09 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,9 @@
-function status (request, response) {
-    response.status(200).json({ message: "tudo certo" })
+import database from "../../../../infra/database.js";
 
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1;  ");
+  console.log(result);
+  response.status(200).json({ message: "tudo certo" });
 }
 
-export default status
+export default status;
