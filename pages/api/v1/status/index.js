34dc6f9dd626a8a95d69b239fd550d081e5e4ecc
Marcus Vinicius Assunção Silva <114960665+MarcusVinicius14@users.noreply.github.com>
2023-11-21 13:16:30 +0000

    add `jsconfig.json`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,9 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;  ");
-  console.log(result);
-  response.status(200).json({ message: "tudo certo" });
+  const result = await database.query("SELECT 1 + 1 as sum");
+  console.log(result.rows);
+  response.status(200).json({ chave: "são acima da media" });
 }
 
 export default status;
