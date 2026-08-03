51bf8b3cec3b9948722d433e508346198cde8ccf
Marcus Vinicius Assunção Silva <114960665+MarcusVinicius14@users.noreply.github.com>
2026-08-03 18:40:05 +0000

    handles database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -1,12 +1,14 @@
 import { Client } from "pg";
 
 async function query(queryRequest) {
+  console.log(process.env.NODE_ENV);
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
   try {
     await client.connect();
